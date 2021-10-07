c2a9a849181e8b9d5a8cbd1193f96f63a4f92dfa
Joshua Andes <andesjoshua@comcast.net>
2021-10-07 17:30:00 -0400

    commit tree 21d6f95e760869b8ac7d72a9ec98a9d5581eafb8

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+function createEmployeeRecord(employeeInfo) {
+    let employeeObj = {
+        firstName: employeeInfo[0],
+        familyName: employeeInfo[1],
+        title: employeeInfo[2],
+        payPerHour: employeeInfo[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return employeeObj
+}
+
+function createEmployeeRecords(infoArray) {
+    return infoArray.map(array => createEmployeeRecord(array));
+}
+
+function createTimeInEvent(recordObj, date) {
+    
+   recordObj.timeInEvents.push({
+        type:'TimeIn',
+        hour: Number(date.slice(-4)),
+        date: date.slice(0, 10),
+    })
+   return recordObj
+}
+function createTimeOutEvent(recordObj, date) {
+    
+    recordObj.timeOutEvents.push({
+         type:'TimeOut',
+         hour: Number(date.slice(-4)),
+         date: date.slice(0, 10),
+     })
+    return recordObj
+ }
+
+ function hoursWorkedOnDate(recordObj, date) {
+    let timeIn = createTimeInEvent(recordObj, date);
+    let timeOut = createTimeOutEvent(recordObj, date);
+    console.log(timeOut.timeOutEvents[0].hour)
+    const hoursWorked = timeOut.timeOutEvents[0].hour - timeIn.timeInEvents[0].hour
+    return hoursWorked
+ }
+
